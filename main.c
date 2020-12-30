e7c0c3fac885958492ab547948524cefecbf90b1
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2020-12-30 18:29:43 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operands.h"
+
+ //Copyright by Fabio-Git-Dev - Software Engineer student UniBa
+
+
+    unsigned int choice; //Used to choose unit at the beginning
+
+    unsigned int decision;  //Used to chose option at the end of operation.
+
+
+
+   /*NOTE, all the inputs that require a scanf that are depending from a do-while or a if-else ecc, MUST be declared in the same place
+    where the cicles are declared, or else the condition won't be read and it won't work*/
+
+
+
+
+
+
+int main(void) //Main function
+
+{
+
+
+         printf("Calculator by Fabio-Git-Dev \n");
+
+
+
+   do{
+
+
+    printf("Wich operation you want to do? 1(+) 2(-) 3(x) 4(/) 5 and others(Exit) \n");
+
+    scanf("%d", &choice);
+
+
+    /*Conditions in case the number doesn't correspond to the number given in the selection
+      for each number a condition is needed, that's why need to use else if to express a condition.*/
+
+
+    if (choice == 6) {
+
+        printf("Wrong selection, retry\n");
+
+        return main();
+    }
+
+       else if (choice == 7) {
+
+        printf("Wrong selection, retry\n");
+
+        return main();
+
+
+       } else if (choice == 8) {
+
+        printf("Wrong selection, retry\n");
+
+        return main();
+
+
+       }  else if (choice == 9) {
+
+        printf("Wrong selection, retry\n");
+
+        return main();
+
+       }
+
+
+
+
+switch (choice) { //switch case for operand choice.
+
+
+
+case 1:
+
+     do{
+
+         sum();
+
+          printf("Do you want to continue with this format? 1(Yes) 2(No) \n");
+          scanf("%d", &decision);
+
+           system("cls");
+
+
+   } while (decision == 1);
+     break;
+
+
+
+
+
+ case 2:
+
+     do {
+
+         sub();
+
+          printf("Do you want to continue with this format? 1(Yes) 2(No) \n");
+          scanf("%d", &decision);
+
+           system("cls");
+
+
+    } while (decision == 1);
+      break;
+
+
+
+
+
+
+
+ case 3:
+
+    do {
+            mult();
+
+             printf("Do you want to continue with this format? 1(Yes) 2(No) \n");
+          scanf("%d", &decision);
+
+           system("cls");
+
+
+    } while (decision == 1);
+      break;
+
+
+
+
+ case 4:
+     do {
+
+          divis();
+
+           printf("Do you want to continue with this format? 1(Yes) 2(No) \n");
+          scanf("%d", &decision);
+
+           system("cls");
+
+
+         } while (decision == 1);
+           break;
+
+
+
+
+
+
+
+default:
+
+
+            printf("Exiting program... \n");
+            system("pause"); //Value used to exit program.
+
+            return 0;
+
+
+
+
+
+}
+
+
+} while (choice == 1);
+
+
+
+}
+
+
+
+
+
+ //end of function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ operands.c
@@ -0,0 +1,131 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include "operands.h"
+
+//DON'T PUT CONDITION VALUES THAT HAS TO BE READ IN THE MAIN HERE! OR ELSE THE VALUE FOR THE OCNDITION WILL FAIL TO BE VERIFIED.
+
+void divis() {
+
+
+    float div;
+
+    float n1;
+    float n2;
+
+    char exclude = '^,^';
+
+
+
+
+
+         printf("Insert two numbers \n");
+         scanf("%f %f", &n1, &n2);
+
+         if (n1 && n2 == exclude) {
+
+            printf("wrong symbol, use the correct period symbol \n");
+
+            return divis();
+         }
+
+
+         div = n1 / n2;
+
+
+         printf("%.2f \n", div);
+
+
+
+}
+
+
+void sub() {
+
+
+    float sub;
+
+    float n1;
+    float n2;
+
+
+
+
+
+        printf("Insert two numbers \n");
+         scanf("%f %f", &n1, &n2);
+
+
+
+         sub = n1 - n2;
+
+
+         printf("%.2f \n", sub);
+
+
+
+
+}
+
+void sum() {
+
+
+    float sum;
+
+    float n1;
+    float n2;
+
+
+
+
+
+         printf("Insert two numbers \n");
+         scanf("%f %f", &n1, &n2);
+
+
+
+         sum = n1 + n2;
+
+
+         printf("%.2f \n", sum);
+
+
+
+
+}
+
+
+void mult() {
+
+
+    float mult;
+
+    float n1;
+    float n2;
+
+
+
+
+
+        printf("Insert two numbers \n");
+         scanf("%f %f", &n1, &n2);
+
+
+
+         mult = n1 * n2;
+
+
+         printf("%.2f \n", mult);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
